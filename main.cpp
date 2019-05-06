ff40884cf612388bea9329c6e0f356fd8fbafc66
YT <842420717@qq.com>
2019-05-06 15:56:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	impl.getRequest()->setHeader("Accept-Language:en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7\r\n");
 	impl.getRequest()->setHeader("Accept-Encoding:gzip, deflate, br\r\n");
 	impl.getRequest()->setHeader("User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\r\n");
-	auto reply = impl.syncGet("http://www.qaqgame.com/");
+	auto reply = impl.syncGet("http://www.baidu.com/");
 
 	if (reply->getError() == YRE_NOERROR)
 	{
